d43234eabf33d7c9b040ff7a978236cc44143c55
Igor <tak.prosto2013@yandex.ru>
2019-04-10 20:09:19 +0300

    it's work

--- 1-mpi/main-deb.cpp
+++ 1-mpi/main-deb.cpp
@@ -169,38 +169,113 @@ int main(int argc, char *argv[])
     double **locMat = makeArray2D(M + 2, N + 2);
     double u0, lMax, dMax;
 
+    DEBUG_FILE << "Matrix U: init" << std::endl;
+    for (size_t i = 0; i < N + 2; i++)
+    {
+        for (size_t j = 0; j < N + 2; j++)
+        {
+            DEBUG_FILE << std::setw(2) << u[i][j] << " ";
+        }
+        DEBUG_FILE << std::endl;
+    }
+
+    DEBUG_FILE << "ProcSize: " << ProcSize << std::endl;
+    DEBUG_FILE << "M: " << M << std::endl;
+    DEBUG_FILE << "sendcounts_Scatterv: ";
+    for (size_t i = 0; i < ProcSize; i++)
+        DEBUG_FILE << sendcounts_Scatterv[i] << "(" << sendcounts_Scatterv[i] / (N + 2) << ") ";
+    DEBUG_FILE << std::endl;
+
+    DEBUG_FILE << "displs_Scatterv: ";
+    for (size_t i = 0; i < ProcSize; i++)
+        DEBUG_FILE << displs_Scatterv[i] << "(" << displs_Scatterv[i] / (N + 2) << ") ";
+    DEBUG_FILE << std::endl;
+
+    DEBUG_FILE << "sendcounts_Gatherv: ";
+    for (size_t i = 0; i < ProcSize; i++)
+        DEBUG_FILE << sendcounts_Gatherv[i] << "(" << sendcounts_Gatherv[i] / (N + 2) << ") ";
+    DEBUG_FILE << std::endl;
+
+    DEBUG_FILE << "displs_Gatherv: ";
+    for (size_t i = 0; i < ProcSize; i++)
+        DEBUG_FILE << displs_Gatherv[i] << "(" << displs_Gatherv[i] / (N + 2) << ") ";
+    DEBUG_FILE << std::endl;
+
+    int iter = 1;
     do
     {
         lMax = 0.0;
+        DEBUG_FILE << "_0_ Iteration: " << iter << std::endl;
+
         MPI_Scatterv(&(u[0][0]), sendcounts_Scatterv, displs_Scatterv, MPI_DOUBLE, &(locMat[0][0]), sendcounts_Scatterv[0], MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
+        DEBUG_FILE << "_1_ LocalMatrix:\n";
+        for (size_t i = 0; i < M + 2; i++)
+        {
+            for (size_t j = 0; j < N + 2; j++)
+            {
+                DEBUG_FILE << std::setw(2) << locMat[i][j] << " ";
+            }
+            DEBUG_FILE << std::endl;
+        }
+
+        // Calculate
         for (size_t i = 1; i < M + 1; i++)
             for (size_t j = 1; j < N + 1; j++)
             {
                 u0 = locMat[i][j];
                 locMat[i][j] = 0.25 * (locMat[i - 1][j] + locMat[i + 1][j] + locMat[i][j - 1] + locMat[i][j + 1]); // - h * h * f[i - 1][j - 1]);
+                //locMat[i][j] = ProcRank - ProcSize;
                 lMax = std::max(std::fabs(u0 - locMat[i][j]), lMax);
             }
+        DEBUG_FILE << "_2_\n";
 
         MPI_Gatherv(&(locMat[0][0]) + (N + 2), sendcounts_Gatherv[0], MPI_DOUBLE, &(u[0][0]), sendcounts_Gatherv, displs_Gatherv, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
         MPI_Reduce(&lMax, &dMax, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
         MPI_Bcast(&dMax, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
+        DEBUG_FILE << "_3_ Matrix U after MPI_Gatherv:\n";
+        for (size_t i = 0; i < N + 2; i++)
+        {
+            for (size_t j = 0; j < N + 2; j++)
+            {
+                DEBUG_FILE << std::setw(2) << u[i][j] << " ";
+            }
+            DEBUG_FILE << std::endl;
+        }
+
+        DEBUG_FILE << "_4_ MPI_Barrier\n";
         MPI_Barrier(MPI_COMM_WORLD);
+        DEBUG_FILE << "_5_\n";
+        DEBUG_FILE << "lMax: " << lMax << std::endl;
+        DEBUG_FILE << "dMax: " << dMax << std::endl;
+        iter++;
+        DEBUG_FILE << "_6_\n";
     } while (dMax > eps);
 
+    DEBUG_FILE << "make result.txt ...\n";
     if (ProcRank == 0)
         toFile(u, N + 2, N + 2);
 
+    DEBUG_FILE << "Clean mermory ...\n";
+
     delete[] displs_Scatterv;
+    DEBUG_FILE << " displs_Scatterv\n";
     delete[] displs_Gatherv;
+    DEBUG_FILE << " displs_Gatherv\n";
     delete[] sendcounts_Scatterv;
+    DEBUG_FILE << " sendcounts_Scatterv\n";
     delete[] sendcounts_Gatherv;
+    DEBUG_FILE << " sendcounts_Gatherv\n";
     freeArray2D(locMat, M + 2, N + 2);
+    DEBUG_FILE << " locMat\n";
     freeArray2D(f, N, N);
+    DEBUG_FILE << " f\n";
     freeArray2D(u, N + 2, N + 2);
+    DEBUG_FILE << " u\n";
 
     MPI_Finalize();
+
     return 0;
 }
