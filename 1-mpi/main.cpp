2bdbb38dfbcbd04888e545bfa9549e255ee657a0
Igor <tak.prosto2013@yandex.ru>
2019-04-10 18:33:16 +0300

    debug

--- 1-mpi/main.cpp
+++ 1-mpi/main.cpp
@@ -4,6 +4,7 @@
 #include <cmath>
 #include <algorithm>
 #include <unistd.h>
+#include <fstream>
 
 double function(const double x, const double y)
 {
@@ -90,10 +91,24 @@ void print2D(double **array2D, size_t rows, size_t cols)
     }
 }
 
+void ToCSV(double **array2D, size_t rows, size_t cols)
+{
+    std::ofstream outStream("result.txt", std::ios_base::out);
+
+    for (size_t i = 0; i < rows; ++i)
+    {
+        for (size_t j = 0; j < cols; ++j)
+            outStream << array2D[i][j] << ",";
+        outStream << std::endl;
+    }
+    outStream.close();
+}
+
 int main(int argc, char *argv[])
 {
+    MPI_Init(&argc, &argv);
     int ProcRank, ProcSize;
-    size_t N = 8;
+    size_t N = 1000;
     double h = step(N);
     double eps = 0.0001;
 
@@ -117,9 +132,10 @@ int main(int argc, char *argv[])
         }
     }
 
-    MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &ProcRank);
     MPI_Comm_size(MPI_COMM_WORLD, &ProcSize);
+
+    std::ofstream DEBUG_FILE("out-" + std::to_string(ProcRank) + ".txt", std::ios_base::out);
 
     double **f = 0;
     double **u = 0;
@@ -129,14 +145,14 @@ int main(int argc, char *argv[])
 
     if (ProcRank == 0)
     {
-        std::cout << "ProcSize: " << ProcSize << std::endl;
+        // std::cout << "ProcSize: " << ProcSize << std::endl;
 
         // f = makeArray2D(N, N);
         // u = makeArray2D(N + 2, N + 2);
 
         first_approx_f(f, N, h);
         first_approx_u(u, N, h);
-        print2D(u, N + 2, N + 2);
+        // print2D(u, N + 2, N + 2);
     }
 
     const int M = N / ProcSize;
@@ -148,20 +164,51 @@ int main(int argc, char *argv[])
         displs[i] = (i == 0) ? 0 : (i * L - 2 * (N + 2));
         sendcounts[i] = L;
     }
-
     double **locMat = makeArray2D(M + 2, N + 2);
-
     double u0 = 0.0, delta = 0.0;
     double dMax = 0.0;
 
+    DEBUG_FILE << "Matrix U: init" << std::endl;
+    for (size_t i = 0; i < N + 2; i++)
+    {
+        for (size_t j = 0; j < N + 2; j++)
+        {
+            DEBUG_FILE << std::setw(2) << u[i][j] << " ";
+        }
+        DEBUG_FILE << std::endl;
+    }
+
+    DEBUG_FILE << "ProcSize: " << ProcSize << std::endl;
+    DEBUG_FILE << "M: " << M << std::endl;
+    DEBUG_FILE << "L: " << L << std::endl;
+    DEBUG_FILE << "sendcounts: ";
+    for (size_t i = 0; i < ProcSize; i++)
+        DEBUG_FILE << sendcounts[i] << " ";
+    DEBUG_FILE << std::endl;
+
+    DEBUG_FILE << "displs: ";
+    for (size_t i = 0; i < ProcSize; i++)
+        DEBUG_FILE << displs[i] << " ";
+    DEBUG_FILE << std::endl;
+
+    int iter = 1;
     do
     {
-        if (ProcRank == 3)
-            std::cout << "0 " << ProcRank << std::endl;
+        DEBUG_FILE << "_0_ Iteration: " << iter << std::endl;
+
         MPI_Scatterv(&(u[0][0]), sendcounts, displs, MPI_DOUBLE, &(locMat[0][0]), L, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+        DEBUG_FILE << "_1_ LocalMatrix:\n";
+        for (size_t i = 0; i < M + 2; i++)
+        {
+            for (size_t j = 0; j < N + 2; j++)
+            {
+                DEBUG_FILE << std::setw(2) << locMat[i][j] << " ";
+            }
+            DEBUG_FILE << std::endl;
+        }
+
         // Calculate
-        if (ProcRank == 3)
-            std::cout << "1 " << ProcRank << std::endl;
         for (size_t i = 1; i < M + 1; i++)
             for (size_t j = 1; j < N + 1; j++)
             {
@@ -169,35 +216,37 @@ int main(int argc, char *argv[])
                 locMat[i][j] = 0.25 * (locMat[i - 1][j] + locMat[i + 1][j] + locMat[i][j - 1] + locMat[i][j + 1]); // - h * h * f[i - 1][j - 1]);
                 delta = std::max(std::fabs(u0 - locMat[i][j]), delta);
             }
-        if (ProcRank == 3)
-            std::cout << "2 " << ProcRank << std::endl;
+        DEBUG_FILE << "_2_\n";
 
-        MPI_Barrier(MPI_COMM_WORLD);
         MPI_Reduce(&delta, &dMax, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
-        MPI_Barrier(MPI_COMM_WORLD);
         MPI_Bcast(&dMax, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-        MPI_Barrier(MPI_COMM_WORLD);
         MPI_Gatherv(&(locMat[0][0]), L, MPI_DOUBLE, &(u[0][0]), sendcounts, displs, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-        // if (ProcRank == 0)
-        // {
-        //     std::cout << "D: " << dMax << std::endl;
-        //     print2D(u, N + 2, N + 2);
-        //     std::cout << "__________________" << std::endl;
-        // }
-        if (ProcRank == 3)
-            std::cout << "3 " << ProcRank << std::endl;
+        DEBUG_FILE << "_3_\n";
+
+        DEBUG_FILE << "Matrix U after MPI_Gatherv:\n";
+        for (size_t i = 0; i < N + 2; i++)
+        {
+            for (size_t j = 0; j < N + 2; j++)
+            {
+                DEBUG_FILE << std::setw(2) << u[i][j] << " ";
+            }
+            DEBUG_FILE << std::endl;
+        }
+        DEBUG_FILE << "_4_ MPI_Barrier\n";
         MPI_Barrier(MPI_COMM_WORLD);
-        if (ProcRank == 3)
-            std::cout << "4 " << ProcRank << std::endl;
+        DEBUG_FILE << "_5_\n";
+        iter++;
     } while (false);
+    DEBUG_FILE << "Clean mermory... " << std::endl;
     // Clean memoryp
-    // delete[] displs;
-    // delete[] sendcounts;
-    // freeArray2D(locMat, M + 2, N + 2);
-    // freeArray2D(f, N, N);
-    // freeArray2D(u, N + 2, N + 2);
+    delete[] displs;
+    delete[] sendcounts;
+    freeArray2D(locMat, M + 2, N + 2);
+    freeArray2D(f, N, N);
+    freeArray2D(u, N + 2, N + 2);
 
     MPI_Finalize();
 
+    //ToCSV(u, N + 2, N + 2);
     return 0;
 }
